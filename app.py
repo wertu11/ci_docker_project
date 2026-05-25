2ad437154331da591f99aecb0ecd3349f55293d1
Wertu <wertu1505@yandex.ru>
2026-05-25 20:07:01 +0300

    add code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return 'Hello, Docker CI/CD!'
 
+@app.route('/health')
+def health():
+    return jsonify(status='ok')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
